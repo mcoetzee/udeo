4bc25213d0aa07918833d82b1ad0291afdbc7774
Markus Coetzee <mks.coetzee@gmail.com>
2016-05-29 22:12:02 +0200

    WIP Add some more createStore tests

--- test/createStore-spec.js
+++ test/createStore-spec.js
@@ -2,47 +2,160 @@
 import { expect } from 'chai';
 import { createStore } from '../';
 
+const FOO = '@test/FOO';
+const BAR = '@test/BAR';
+
+const noopModule = {
+  flow: dispatch$ => [dispatch$],
+  reducer: (state, action) => state,
+};
+
+function fooFlow(dispatch$) {
+  return [
+    dispatch$.filterAction(FOO)
+  ];
+}
+
+function barFlow(dispatch$) {
+  return [
+    dispatch$.filterAction(BAR)
+  ];
+}
+
 describe('createStore', () => {
+  it('exposes the public API', () => {
+    const store = createStore({ noopModule });
+
+    expect(Object.keys(store)).to.have.length(6);
+    expect(store.getState$).to.be.a('function');
+    expect(store.dispatch).to.be.a('function');
+    expect(store.setMiddleware).to.be.a('function');
+    expect(store.hydrate).to.be.a('function');
+    expect(store.clearState).to.be.a('function');
+    expect(store.addStreams).to.be.a('function');
+  });
+
+  it('gets initial state from reducer', () => {
+    const initialState = { valueA: 10 };
+    const fooModule = {
+      flow: dispatch$ => [dispatch$],
+      reducer: (state = initialState, action) => state
+    };
+
+    const store = createStore({ fooModule });
+
+    let fooState;
+    const sub = store.getState$('fooModule').subscribe(state => {
+      fooState = state;
+    });
+
+    expect(fooState).to.deep.eq({ valueA: 10 });
+    sub.unsubscribe();
+  });
+
   describe('dispatch', () => {
     it('reaches all active streams', () => {
-      const moduleOne = {
-        flow(dispatch$) {
-          return [dispatch$];
-        },
-        reducer(state = [], action) {
-          return state.concat(action.type);
-        },
-      };
-      const moduleTwo = {
-        flow(dispatch$) {
-          return [dispatch$];
-        },
-        reducer(state = [], action) {
-          return state.concat(action.type);
+      const fooModule = {
+        flow: fooFlow,
+        reducer(state = [], { type }) {
+          switch (type) {
+            case FOO:
+              return state.concat(type);
+            default:
+              return state;
+          }
         },
       };
+      const barModule = {
+        flow: barFlow,
+        reducer(state = [], { type }) {
+          switch (type) {
+            case BAR:
+              return state.concat(type);
+            default:
+              return state;
+          }
+        },
+      };
+
       const store = createStore({
-        moduleOne,
-        moduleTwo,
+        fooModule,
+        barModule,
       });
 
-      let moduleOneState;
-      const subOne = store.getState$('moduleOne').subscribe(state => {
-        moduleOneState = state;
+      let fooState;
+      const subOne = store.getState$('fooModule').subscribe(state => {
+        fooState = state;
       });
 
-      let moduleTwoState;
-      const subTwo = store.getState$('moduleTwo').subscribe(state => {
-        moduleTwoState = state;
+      let barState;
+      const subTwo = store.getState$('barModule').subscribe(state => {
+        barState = state;
       });
 
-      store.dispatch({ type: '@test/FOO' });
-      store.dispatch({ type: '@test/BAR' });
+      expect(fooState).to.deep.equal([]);
+      expect(barState).to.deep.equal([]);
 
-      expect(moduleOneState).to.deep.equal(['@udeo/INIT', '@test/FOO', '@test/BAR']);
-      expect(moduleTwoState).to.deep.equal(['@udeo/INIT', '@test/FOO', '@test/BAR']);
+      store.dispatch({ type: FOO });
+
+      expect(fooState).to.deep.equal([FOO]);
+      expect(barState).to.deep.equal([]);
+
+      store.dispatch({ type: BAR });
+
+      expect(fooState).to.deep.equal([FOO]);
+      expect(barState).to.deep.equal([BAR]);
+
       subOne.unsubscribe();
       subTwo.unsubscribe();
     });
+  });
+
+  it('allows state stream access when composing action streams', () => {
+    const fooModule = {
+      flow(dispatch$, { getState$ }) {
+        const bar$ = getState$('barModule').pluck('bar');
+
+        const foo$ = dispatch$
+          .filterAction(FOO)
+          .withLatestFrom(bar$, (action, bar) => action.payload * bar)
+          .mapAction(FOO);
+
+        return [foo$];
+      },
+      reducer(state = [], { type, payload }) {
+        switch (type) {
+          case FOO:
+            return state.concat(payload);
+          default:
+            return state;
+        }
+      }
+    };
+
+    const barModule = {
+      flow: barFlow,
+      reducer(state = { bar: 2 }, action) {
+        return state;
+      }
+    };
+
+    const store = createStore({
+      fooModule,
+      barModule,
+    });
+
+    let fooState;
+    const subOne = store.getState$('fooModule').subscribe(state => {
+      fooState = state;
+    });
+
+    store.dispatch({ type: FOO, payload: 10 });
+    expect(fooState).to.deep.eq([20]);
+
+    store.dispatch({ type: FOO, payload: 21 });
+    expect(fooState).to.deep.eq([20, 42]);
+
+    subOne.unsubscribe();
   });
 });
